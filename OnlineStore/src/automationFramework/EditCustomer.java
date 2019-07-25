89cbd387216ba6ec2e7ac7cf8b293f67d61aab72
srinivas1024 <chevvasrinivas@gmail.com>
2019-07-25 21:39:33 +0530

    New test case added

---
+++ OnlineStore/src/automationFramework/EditCustomer.java
@@ -0,0 +1,11 @@
+package automationFramework;
+
+public class EditCustomer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
