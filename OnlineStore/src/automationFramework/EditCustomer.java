89822d5c3fc85b7541203d7eb3e51edd7c37fc5f
srinivas1024 <chevvasrinivas@gmail.com>
2019-07-25 21:44:49 +0530

    Edit customer modified

--- OnlineStore/src/automationFramework/EditCustomer.java
+++ OnlineStore/src/automationFramework/EditCustomer.java
@@ -5,6 +5,12 @@ public class EditCustomer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		System.out.println("Hello world");
+
+		System.out.println("Hello world");
+
+		System.out.println("Hello world");
+
 
 	}
 
